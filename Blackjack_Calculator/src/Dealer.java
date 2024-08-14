4bbe52b8c935e4b63943585c6a3a3186c34bef05
Owen <owenjgibson@gmail.com>
2024-08-14 16:52:50 +0100

    Adding functionality to player

--- Blackjack_Calculator/src/Dealer.java
+++ Blackjack_Calculator/src/Dealer.java
@@ -1,3 +1,24 @@
+import java.util.List;
+
 public class Dealer{
 
+    List<Deck> decks;
+
+    public Dealer(List<Deck> decks){
+        this.decks = decks;
+    }
+
+    public void shuffleDecks(List<Deck> decks){
+        for (Deck d: decks) {
+            d.shuffleDeck();
+        }
+    }
+
+    public void dealCard(Player p){
+        p.addCardToHand(decks.get(0).getCard());
+    }
+    
+    public void play(){
+
+    }
 }
--- Blackjack_Calculator/src/Player.java
+++ Blackjack_Calculator/src/Player.java
@@ -2,10 +2,66 @@ public class Player {
 
     Hand playerHand;
     int playerBalance = 100;
+    int currentBet;
 
     public Player(int balance){
         playerBalance = balance;
     }
 
+    public int getPlayerBalance(){
+        return playerBalance;
+    }
 
+    public void setPlayerBalance(int playerBalance) {
+        this.playerBalance = playerBalance;
+    }
+
+    public void placeBet(int amount){
+        if(amount <= playerBalance){
+            removeBalance(amount);
+            currentBet = amount;
+        }else {
+            System.out.println("ERROR: Can't bet more than you have available");
+        }
+    }
+
+    public void winBet(){
+        playerBalance += currentBet *2;
+        currentBet = 0;
+    }
+
+    public void loseBet(){
+        currentBet = 0;
+    }
+
+    public void addBalance(int amount){
+        this.playerBalance += amount;
+    }
+    public void removeBalance(int amount){
+        this.playerBalance -= amount;
+    }
+
+    public void addCardToHand(Card c){
+        playerHand.addCard(c);
+    }
+
+    public void clearHand(){
+        playerHand.clearHand();
+    }
+
+    public void hit(){
+
+    }
+
+    public void stand(){
+
+    }
+
+    public void split(){
+
+    }
+
+    public void doubleDown(){
+
+    }
 }
